4fcf7ca66e8e728ac1387b4f5038fdf9579eedd9
Robert <robert.hostler@protonmail.com>
2019-11-22 13:23:45 -0500

    updated the readme and the credentials file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 I have used the simple TCP server and client I wrote previously to create a simplified clone of the SSH tool.
 Once users of the client script log in, they can run a bash command that is then sent to the server and
 executed there.
+This is obviously not meant to be a serious alternative to SSH; I did it for fun and for educational purposes.
+If you want to try it for yourself, the username is 'admin' and the password is 'password'.
--- ssh_clone_server.py
+++ ssh_clone_server.py
@@ -1,23 +1,79 @@
 import socket
+import crypt
+import os
+
+
+def client_input(client, prompt):
+    client.send(prompt.encode("utf-8"))
+    client_response = client.recv(1024).decode("utf-8")
+    return client_response
+
+def login(client, socket):
+    password_tries = 3
+    while password_tries > 0:
+        username = client_input(client, "Please enter your username:")
+        password = client_input(client, "Please enter your password:")
+
+        if username_exists(username) and password_matches(username, password):
+            return True
+        else:
+            client.send("Your username or password is incorrect. Try again.\n".encode("utf-8"))
+            password_tries -= 1
+
+    client.send("q".encode("utf-8"))
+    client.close()
+    socket.close()
+    quit()
+
+def username_exists(username):
+    with open("credentials.txt", "r") as credentials:
+        for line in credentials.readlines():
+            components = line.split(":")
+            savedusername = components[0].strip()
+
+            if username == savedusername:
+                return True
+        else:
+            return False
+
+def password_matches(username, password):
+    password_hash = crypt.crypt(password, salt='METHOD_BLOWFISH')
+    with open("credentials.txt", "r") as credentials:
+        for line in credentials.readlines():
+            components = line.split(":")
+            savedusername = components[0].strip()
+            savedhash = components[1].strip()
+
+            if username == savedusername:
+                if password_hash == savedhash:
+                    return True
+        else:
+            return False
 
 def main():
-    host, port = "127.0.0.1", 5000
+    host, port = "127.0.0.1", 6000
     sock = socket.socket()
-    sock.connect((host, port))
+    sock.bind((host, port))
+    sock.listen(1)
+    client, addr = sock.accept()
+    print("Connection from: " + str(addr))
+
+    password_hash = crypt.crypt("password", salt='METHOD_BLOWFISH')
+    print(password_hash)
+
+    if not login(client, "Please log in to the SSH server:"):
+        quit()
 
     while True:
-        message = sock.recv(1024).decode("utf-8")
-
-        if message == "q":
+        bash_command = client_input(client, "user@SSHclone:" + os.getcwd())
+        if bash_command == "q":
             break
         else:
-            response = input(message + "\n=> ")
-            if response == "q":
-                sock.send(response.encode("utf-8"))
-                break
-            else:
-                sock.send(response.encode("utf-8"))
+            os.system(bash_command)
+
+    client.close()
     sock.close()
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
